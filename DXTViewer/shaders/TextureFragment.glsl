f2fdb48ec2245de34a58799dcd9b4b231f0f5b47
jmacey <jmacey@bournemouth.ac.uk>
2014-10-02 22:10:40 +0100

    modified the shader names and uploaded

---
+++ DXTViewer/shaders/TextureFragment.glsl
@@ -0,0 +1,14 @@
+#version 400 core
+/// @brief our output fragment colour
+layout (location =0) out vec4 fragColour;
+// texture sampler
+uniform sampler2D tex;
+// uv from input
+in vec2 vertUV;
+
+// note the texture is compressed in this example as bgr
+void main ()
+{
+  fragColour.rgb=texture(tex,vertUV ).bgr;
+}
+
